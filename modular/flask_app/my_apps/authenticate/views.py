295333992bc3be8429ecc2c74740737d56e6f75d
Deiver Vazquez <dvazquez@lsv-tech.com>
2023-05-02 15:34:50 -0500

    finished base structure

--- modular/flask_app/my_apps/authenticate/views.py
+++ modular/flask_app/my_apps/authenticate/views.py
@@ -37,43 +37,17 @@ class AuthenticateView(MethodView):
         current_app.logger.debug(f'access_token: {access_token}')
         return Response(json.dumps({"message": 'Login successful', "token": access_token, "data": user}), mimetype='application/json', status=200)
 
-# @login_app.route('/login/', methods=['POST'])
-# @cross_origin(supports_credentials=True)
-# def login_endpoint():
-#     data = request.json
-#     email = data['email']
-#     password = data['password']
+    @jwt_required()
+    @cross_origin(supports_credentials=True)
+    def get(self):
+        session.pop('user', None)
+        session.pop('token', None)
+        current_app.logger.debug(f'session: {session}')
+        return Response(json.dumps({"message": 'Logout successful'}), mimetype='application/json', status=200)
 
-#     user = {
-#         "email": 'admin@lsv-tech.com',
-#         "password": 'admin1213'
-#     }
-
-#     if user['email'] != email:
-#         return Response(json.dumps({"message": 'Invalid email'}), mimetype='application/json', status=500)
-
-#     if user['password'] != password:
-#         return Response(json.dumps({"message": 'Invalid password'}), mimetype='application/json', status=500)
-
-#     access_token = create_access_token(identity=email)
-
-#     session['user'] = user
-#     session['token'] = access_token
-
-#     current_app.logger.debug(f'access_token: {access_token}')
-#     return Response(json.dumps({"message": 'Login successful', "token": access_token, "data": user}), mimetype='application/json', status=200)
-
-
-# @login_app.route('/logout/', methods=['POST'])
-# @jwt_required()
-# @cross_origin(supports_credentials=True)
-# def logout():
-#     session.pop('user', None)
-#     session.pop('token', None)
-#     current_app.logger.debug(f'session: {session}')
-#     return Response(json.dumps({"message": 'Logout successful'}), mimetype='application/json', status=200)
 
 authenticate_view = AuthenticateView.as_view('authenticate_view')
 
 # adding routes
 authenticate_app.add_url_rule('/api/v1/authenticate/', view_func=authenticate_view, methods=['POST'])
+authenticate_app.add_url_rule('/api/v1/logout/', view_func=authenticate_view, methods=['GET'])
